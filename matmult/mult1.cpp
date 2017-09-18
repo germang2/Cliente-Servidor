760d8b33da9b053652676a06abd519fabb306384
utp <utp@utp>
2017-09-18 08:56:58 -0500

    Adds version of n*n threads and n threads

--- matmult/mult1.cpp
+++ matmult/mult1.cpp
@@ -2,12 +2,19 @@
 #include "graphreader.hh"
 #include <string>
 #include <cassert>
-#include "timer.hh"
+#include <thread>
+#include <vector>
 
 using namespace std;
 using Mat = vector<vector<int>>;
 
-void mult0(const Mat &m1, const Mat &m2, Mat &res){
+void dot(const Mat &m1, const Mat &m2, int a, int b, int &res){
+    int j = m1[0].size();   //number of cols in m1
+    for(int c = 0; c < j; c++)
+        res += m1[a][c] * m2[c][b];
+}
+
+void mult1(const Mat &m1, const Mat &m2, Mat &res){
     int i = m1.size();      //number of rows in m1
     int j = m1[0].size();   //number of cols in m1
     int k = m2.size();      //number of rows in m2
@@ -15,13 +22,15 @@ void mult0(const Mat &m1, const Mat &m2, Mat &res){
 
     assert(j == k);
 
+    vector<thread> ts;
+
     for(int a = 0; a < i; a++){
         for(int b = 0; b < l; b++){
-            for(int c = 0; c < b; c++){
-                res[a][b] += m1[a][c] * m2[c][b]; 
-            }
+            ts.push_back(thread(dot, cref(m1), cref(m2), a, b, ref(res[a][b])));
         }
     }
+    for(thread &t : ts)
+        t.join();
 }
 
 int main(int argc, char **argv){
@@ -36,10 +45,8 @@ int main(int argc, char **argv){
         res[i].resize(g[0].size());
     }
 
-    {//lack to take the time of execution
-        Timer t("mult0");
-        mult0(g, g, res);
-        cout << t.elapsed() << " ms." << endl;
-    }
+    //lack to take the time of execution
+    mult1(g, g, res);
+
     return 0;
 }
--- matmult/mult2.cpp
+++ matmult/mult2.cpp
@@ -2,12 +2,23 @@
 #include "graphreader.hh"
 #include <string>
 #include <cassert>
+#include <thread>
+#include <vector>
 #include "timer.hh"
 
 using namespace std;
 using Mat = vector<vector<int>>;
 
-void mult0(const Mat &m1, const Mat &m2, Mat &res){
+void dot(const Mat &m1, const Mat &m2, int a, Mat &res){
+    int l = m2[0].size();   //number of cols in m2
+    int j = m1[0].size();   //number of cols in m1
+    for(int b = 0; b < l; b++){
+        for(int c = 0; c < j; c++)
+            res[a][b] += m1[a][c] * m2[c][b];
+    }
+}
+
+void mult2(const Mat &m1, const Mat &m2, Mat &res){
     int i = m1.size();      //number of rows in m1
     int j = m1[0].size();   //number of cols in m1
     int k = m2.size();      //number of rows in m2
@@ -15,14 +26,16 @@ void mult0(const Mat &m1, const Mat &m2, Mat &res){
 
     assert(j == k);
 
+    vector<thread> ts;
+
     for(int a = 0; a < i; a++){
-        for(int b = 0; b < l; b++){
-            for(int c = 0; c < b; c++){
-                res[a][b] += m1[a][c] * m2[c][b]; 
-            }
-        }
+        ts.push_back(thread(dot, cref(m1), cref(m2), a, ref(res)));
     }
+
+    for(thread &t : ts)
+        t.join();
 }
+
 
 int main(int argc, char **argv){
     if(argc != 2){
@@ -36,9 +49,10 @@ int main(int argc, char **argv){
         res[i].resize(g[0].size());
     }
 
-    {//lack to take the time of execution
-        Timer t("mult0");
-        mult0(g, g, res);
+    //lack to take the time of execution
+    {
+        Timer t("mult2");
+        mult2(g, g, res);
         cout << t.elapsed() << " ms." << endl;
     }
     return 0;
